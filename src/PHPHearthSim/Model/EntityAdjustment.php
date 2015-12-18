24163b3feee425069f7abf0eb6f2c65e044cee41
Marius Eliassen <marius@exchange.no>
2015-12-18 15:49:58 +0100

    feat(EntityAdjustment): types and value support for entity adjustment

--- src/PHPHearthSim/Model/EntityAdjustment.php
+++ src/PHPHearthSim/Model/EntityAdjustment.php
@@ -17,5 +17,100 @@ namespace PHPHearthSim\Model;
  */
 class EntityAdjustment {
 
+    /**
+     * This adjustment is used when modifying health up or down by a value
+     *
+     * @var string
+     */
+    const ADJUSTMENT_HEALTH = 'adjustment.health';
+
+    /**
+     * Set health to a specific value
+     *
+     * @var string
+     */
+    const SET_HEALTH = 'set.health';
+
+    /**
+     * This adjustment is used when modifying attack up or down by a value
+     *
+     * @var string
+     */
+    const ADJUSTMENT_ATTACK = 'adjustment.attack';
+
+    /**
+     * Set attack to a specific value
+     *
+     * @var string
+     */
+    const SET_ATTACK = 'set.attack';
+
+    /**
+     * Type of adjustment. See constants defined in this class.
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * The amount/value that has changed. Value format depends on $type
+     *
+     * @var mixed
+     */
+    protected $value;
+
+
+    /**
+     * Construct new adjustment
+     *
+     * @param string $type See constant values
+     * @param mixed $value
+     */
+    public function __construct($type, $value) {
+        $this->type = $type;
+        $this->value = $value;
+    }
+
+    /**
+     * Set adjustment type
+     *
+     * @param string $type
+     * @return \PHPHearthSim\Model\EntityAdjustment
+     */
+    public function setType($type) {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get adjustment type
+     *
+     * @return string
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Set adjustment value
+     *
+     * @param mixed $value
+     * @return \PHPHearthSim\Model\EntityAdjustment
+     */
+    public function setValue($value) {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get adjustment value
+     *
+     * @return mixed
+     */
+    public function getValue() {
+        return $this->value;
+    }
 
 }
