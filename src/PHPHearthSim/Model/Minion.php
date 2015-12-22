fb0ce64397103a44ee014a464d2c22947b8a0339
Marius Eliassen <marius@exchange.no>
2015-12-22 21:09:21 +0100

    feat(Minion): new minion class to extend from

---
+++ src/PHPHearthSim/Model/Minion.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * PHPHearthSim - A Hearthstone simulator written in PHP
+ *
+ * @author:     Switchback <switchback@exchange.no>
+ * @copyright   Copyright (C) 2015 Switchback
+ * @date: 18 12 2015
+ */
+namespace PHPHearthSim\Model;
+
+use PHPHearthSim\Model\Entity;
+
+/**
+ * Minion.
+ * Base class for a minion. Really just here to help check instanceof in other places of the application.
+ *
+ * @class Minion
+ */
+abstract class Minion extends Entity {
+
+}
